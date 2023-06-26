c45a85179bb1bb802b551f0bcc10ab81d2265947
Francisco Angelo <franciscoangelo.feo@gmail.com>
2023-06-26 07:08:04 -0300

    feat: create a module to be responsible to open third party apps

---
+++ apps/applications.go
@@ -0,0 +1,53 @@
+package apps
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"startup/urlFinder"
+	"time"
+)
+
+const (
+	defaultBrowser = "xdg-open"
+	slack          = "slack"
+	intelliJ       = "intellij-idea-ultimate"
+)
+
+func OpenApplications() {
+	fmt.Println("*******************************************************")
+	urls := urlFinder.UrlFinder()
+	openUrls(urls)
+	openSlack()
+	openIntelliJ()
+	fmt.Println("*******************************************************")
+}
+
+func openUrls(urls map[string]string) {
+	for url, name := range urls {
+		fmt.Printf("Opening %s in default browser \n", name)
+		err := exec.Command(defaultBrowser, url).Start()
+		if err != nil {
+			log.Fatal(err)
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+func openSlack() {
+	fmt.Println("Opening Slack!")
+	err := exec.Command(slack).Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	time.Sleep(500 * time.Millisecond)
+}
+
+func openIntelliJ() {
+	fmt.Println("Opening IntelliJ IDEA!")
+	err := exec.Command(intelliJ).Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	time.Sleep(500 * time.Millisecond)
+}
