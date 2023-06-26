e8a228f6dc3f4ce858b04108b2597abcf1678b8c
Francisco Angelo <franciscoangelo.feo@gmail.com>
2023-06-26 07:08:36 -0300

    refactor: clean the main.go function to call a new module

--- main.go
+++ main.go
@@ -2,27 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os/exec"
-	"startup/urlFinder"
-	"time"
+	"startup/apps"
 )
 
 func main() {
 	fmt.Println("Good Morning!")
 
-	urls := urlFinder.UrlFinder()
-	openUrls(urls)
-
-}
-
-func openUrls(urls map[string]string) {
-	for url, name := range urls {
-		fmt.Printf("Opening %s in the default browser \n", name)
-		err := exec.Command("xdg-open", url).Start()
-		if err != nil {
-			log.Fatal(err)
-		}
-		time.Sleep(500 * time.Millisecond)
-	}
+	apps.OpenApplications()
 }
