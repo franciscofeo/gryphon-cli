dec5e69a016d6d0bc4da477ed21a805609c97858
Francisco Angelo <franciscoangelo.feo@gmail.com>
2023-07-01 04:45:37 -0300

    feat: add goroutine in verify APIs process

--- apis/verifyAPIs.go
+++ apis/verifyAPIs.go
@@ -7,17 +7,25 @@ import (
 	"gryphon/apis/slack"
 	"gryphon/apis/util"
 	"log"
+	"sync"
 )
 
 func VerifyAPIs() {
+	var wg sync.WaitGroup
+	wg.Add(3)
+
 	fmt.Println("Verifying APIs:")
 	fmt.Println("---------")
-	checkGithubStatus()
-	checkSlackStatus()
-	checkAtlassianStatus()
+	go checkGithubStatus(&wg)
+	go checkSlackStatus(&wg)
+	go checkAtlassianStatus(&wg)
+
+	wg.Wait()
 }
 
-func checkGithubStatus() {
+func checkGithubStatus(wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	resp, err := github.CheckStatus()
 	if err != nil {
 		log.Fatal("Error when checking Github Status Page informations.")
@@ -26,7 +34,9 @@ func checkGithubStatus() {
 	util.GenerateAPIsInformationMessage(resp)
 }
 
-func checkSlackStatus() {
+func checkSlackStatus(wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	resp, err := slack.CheckStatus()
 	if err != nil {
 		log.Fatal("Error when checking Slack Status Page information.")
@@ -35,7 +45,9 @@ func checkSlackStatus() {
 	util.GenerateAPIsInformationMessage(resp)
 }
 
-func checkAtlassianStatus() {
+func checkAtlassianStatus(wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	resp, err := atlassian.CheckStatus()
 	if err != nil {
 		log.Fatal("Error when checking Atlassian Status Page information.")
